8bb320a5f301bc5429beb662b15203a97261108e
Dibyangana Das <130747608+tupur-29@users.noreply.github.com>
2024-12-29 20:16:58 +0530

    Update Celebrity Problem.py

--- Celebrity Problem.py
+++ Celebrity Problem.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def celebrity(self, mat):
+        # code here
+        top=0
+        down=len(mat)-1
+        while top<down:
+            if mat[top][down]==1:
+                top+=1
+            else:
+                down-=1
+            
+        if top>down:
+            return -1
+        for i in range(len(mat)):
+            if i==top:
+                continue
+            if mat[top][i]==1 or mat[i][top]==0:
+                return -1
+        return top
