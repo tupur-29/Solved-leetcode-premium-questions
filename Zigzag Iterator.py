a6c94423ddedbec655ab20e06d9929a816073910
Dibyangana Das <130747608+tupur-29@users.noreply.github.com>
2025-07-08 12:43:43 +0530

    Update Zigzag Iterator.py

--- Zigzag Iterator.py
+++ Zigzag Iterator.py
@@ -1,1 +1,17 @@
+from collections import deque
+class ZigzagIteratork:
+  def __init__(self,vectors):
+    self.q=deque()
+    self.vectors=vectors
+    for i in range(len(self.vectors)):
+      if self.vectors[i]:
+        self.q.append((i,0,len(self.vectors)))
 
+  def next(self):
+    ind,st,end=self.q.popleft()
+    if st+1<end:
+      self.q.append((ind,st+1,end))
+    return self.vectors[ind][st]
+
+ def hasnext(self):
+   return len(self.q)!=0
